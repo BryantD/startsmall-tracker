41006dc46fc9e4921a7be7f8614d6a9326bda10e
Bryant Durrell <durrell@innocence.com>
2020-04-26 17:04:41 -0700

    Added the rest of the arguments, added some error checking

--- start-tweet.py
+++ start-tweet.py
@@ -39,6 +39,16 @@ from datetime import date
 from credentials import *
 from config import *
 
+def normalize(row):
+	row[0] = row[0].strip()
+	row[1] = row[1].replace(" ", "")
+	row[2] = row[2].strip()
+	row[3] = row[3].strip()
+	row[4] = row[4].strip()
+	row[5] = row[5].strip()
+	
+	return row
+
 def get_donations():
 	r = requests.get(sheet_url)
 	decoded_content = r.content.decode("utf-8")
@@ -54,7 +64,7 @@ def get_donations():
 		elif (row[0] == "Date") and distributed_flag:
 			date_flag = True
 		elif date_flag:
-			trimmed_donations.append(row)
+			trimmed_donations.append(normalize(row))
 			
 	return trimmed_donations
 	
@@ -64,7 +74,6 @@ def make_hash(row):
 	return m.hexdigest()
 	
 def save_donation(db, row, row_hash):
-	print(row)
 	db.insert(
 		{
 			"hash": row_hash,
@@ -78,21 +87,36 @@ def save_donation(db, row, row_hash):
 		}
 	)
 
-def make_text(row):
+def make_text(row, max_length):
 	if (row[0] == ""):
 		row[0] = "None"
 	
 	text = f"Date: {row[0]}\nAmount: {row[1]}\nCategory: {row[2]}\nGrantee: {row[3]}\nLink: {row[4]}\n\n"
 	text += f"Source: {data_source}"
+
+	if len(text) > max_length:
+		text = f"Date: {row[0]}\nAmount: {row[1]}\nGrantee: {row[3]}"
+		
+	if len(text) > max_length:
+		text = f"Amount: {row[1]}\nGrantee: {row[3]}"
+	
 	return text
 
 def main():
 	parser = argparse.ArgumentParser(description='startsmall charity donation tracker')
+	
 	parser.add_argument('--maxlen', default=255, type=int, help='Max tweet length')
 	parser.add_argument('--db', required=True, help='Database location')
-	parser.add_argument('--print', help='Print mixtape', action='store_true')
-	parser.add_argument('--tweet', help='Tweet mixtape', action='store_true')
-	parser.add_argument('--toot', help='Toot mixtape', action='store_true')
+	
+	parser.add_argument('--print', default=True, help='Print donations', action='store_true')
+	parser.add_argument('--tweet', help='Tweet donations', action='store_true')
+	parser.add_argument('--toot', help='Toot donations', action='store_true')
+	
+	parser.add_argument('--new', default=True, help='Get new donations (default)', action='store_true')
+	parser.add_argument('--list', help='Show all recorded donations', action='store_true')
+	parser.add_argument('--delete', help='Hash of a donation to delete', action='store')
+	parser.add_argument('--retrieve', help='Hash of a donation to retrieve', action='store')
+	
 	args = parser.parse_args()
 	
 	db = TinyDB(args.db)
@@ -105,7 +129,7 @@ def main():
 		if not db.search(donation_db.hash == row_hash):
 			save_donation(db, row, row_hash)
 			
-			text = make_text(row)
+			text = make_text(row, args.maxlen)
 			
 			if args.print:
 				print(text)
